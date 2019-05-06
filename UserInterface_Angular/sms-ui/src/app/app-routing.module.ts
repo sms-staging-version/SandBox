82518909b1a70f494581a4c13c5e7ad6e79b849e
Peter Orlando <peter.emmanuel.orlando@gmail.com>
2019-05-06 13:52:28 -0400

    re-upload

---
+++ UserInterface_Angular/sms-ui/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthLoadingComponent } from './components/auth-loading/auth-loading.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'auth-loading',
+    component: AuthLoadingComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ UserInterface_Angular/sms-ui/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthLoadingComponent } from './components/auth-loading/auth-loading.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { CognitoService } from './services/cognito.service';
+import { SmsClientModule } from './sms-client/sms-client.module';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AuthLoadingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    SmsClientModule
+  ],
+  providers: [
+    CognitoService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
